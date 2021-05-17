869d47a7b9eb01eaa22f682cef7552141c77e386
Midnight-Analytics <tobias.finlay@midnight-analytics.com>
2021-05-17 23:40:32 +0100

    Added PancakeSwap Token Search

--- src/pancakeswap.py
+++ src/pancakeswap.py
@@ -1,4 +1,20 @@
+import requests
+
 
 class PancakeSwap(object):
 
-  pass
+  def __init__(self):
+    pass
+
+
+  def get_token_price(self, token):
+
+
+    url = f"https://api.pancakeswap.info/api/v2/tokens/{token}"
+
+    r = requests.get(url)
+
+    return r.json()
+
+  
+
